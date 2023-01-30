47176e4bf2e36eea81ee8abc15c7cca72c8ed2fb
Kuba Jakubowski <kubagp2@gmail.com>
2023-01-30 18:27:41 +0100

    add shared types

---
+++ shared/types.ts
@@ -0,0 +1,87 @@
+// CATEGORY
+
+export type CategoryName = "class" | "teacher" | "classroom";
+export const categoryNames: CategoryName[] = ["class", "teacher", "classroom"];
+
+export type Category = {
+  id: number;
+  name: string;
+}[]
+
+export type Categories = {
+  class: {
+    [key: number]: Category[];
+  },
+  teacher: Category[],
+  classroom: Category[],
+}
+
+// WEEKDAY
+
+export type Weekday =
+  | "monday"
+  | "tuesday"
+  | "wednesday"
+  | "thursday"
+  | "friday";
+
+export const weekdays: Weekday[] = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+];
+
+// CLASSROOM, TEACHER, CLASS
+
+type Entity = {
+  planId: number;
+  shortName: string;
+  longName: string;
+}
+
+export type Classroom = Entity;
+export type Teacher = Entity;
+export type Class = Entity;
+
+// PLAN
+
+export type ClassLesson = {
+  name: string;
+  classroom: Classroom;
+  teacher: Teacher;
+}
+
+export type ClassPlan = {
+  id: number;
+  timetable: {
+    [key in Weekday]: (ClassLesson | null)[][];
+  }
+}
+
+export type TeacherLesson = {
+  name: string;
+  classroom: Classroom;
+  class: Class;
+}
+
+export type TeacherPlan = {
+  id: number;
+  timetable: {
+    [key in Weekday]: (TeacherLesson | null)[][];
+  }
+}
+
+export type ClassroomLesson = {
+  name: string;
+  teacher: Teacher;
+  class: Class;
+}
+
+export type ClassroomPlan = {
+  id: number;
+  timetable: {
+    [key in Weekday]: (ClassroomLesson | null)[][];
+  }
+}
