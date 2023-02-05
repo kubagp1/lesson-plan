030f7b9f261c36df1d39a6e668168bdf2401f985
Kuba Jakubowski <kubagp2@gmail.com>
2023-02-05 01:45:20 +0100

    Copy over api calls

---
+++ frontend/src/apiCalls/index.ts
@@ -0,0 +1,20 @@
+import { Plan, Categories } from '../shared/types'
+
+const entrypoint = import.meta.env.PROD
+  ? location.origin + '/data/'
+  : 'http://localhost:5000/'
+
+const fetchData = async (endpoint: string) => {
+  const response = await fetch(entrypoint + endpoint)
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${endpoint}, received ${response.status}`)
+  }
+  return response.json()
+}
+
+export const getCategories = () =>
+  fetchData('categories') as Promise<Categories>
+
+export const getPlan = (id: number) => fetchData(`plans/${id}`) as Promise<Plan>
+
+export default { getCategories, getPlan }
