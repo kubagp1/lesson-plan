a9811240f007575407f700edda1ee130b8affe57
kubagp <37304664+kubagp1@users.noreply.github.com>
2023-04-17 19:39:41 +0000

    fix typo, add time related funcs, add highlighting current lesson

--- frontend/src/components/WeekdaySlide.tsx
+++ frontend/src/components/WeekdaySlide.tsx
@@ -1,5 +1,5 @@
 import { Table, TableBody, TableCell, TableRow } from '@mui/material'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import type {
   ClassLesson,
   ClassroomLesson,
@@ -24,9 +24,10 @@ type Entry = {
     centerRight: { text: string; planId?: number }
     right: { text: string; planId?: number }
   }[]
+  higlight: boolean
 }
 
-type Entrries = Entry[]
+type Entries = Entry[]
 
 const lessonIsClassroomLesson = (lesson: Lesson): lesson is ClassroomLesson => {
   return (lesson as ClassLesson | TeacherLesson).classroom === undefined
@@ -40,10 +41,74 @@ const lessonIsTeacherLesson = (lesson: Lesson): lesson is TeacherLesson => {
   return (lesson as ClassroomLesson | ClassLesson).teacher === undefined
 }
 
+function hourToMinutes(hour: string): number {
+  const [h, m] = hour.split(':').map((s) => parseInt(s))
+  return h * 60 + m
+}
+
+function shouldBeHighlighted(
+  timeRange: [number, number],
+  previousEnd: number,
+  time: number
+): boolean {
+  const [start, end] = timeRange
+
+  if (previousEnd === -1) return time >= start && time <= end
+
+  if (start === -1 || end === -1) return false
+
+  return time >= previousEnd && time < end
+}
+
+function getTimeRange(str: string): [number, number] {
+  try {
+    const [start, end] = str.split('-')
+
+    const [startHours, startMinutes] = start.split(':').map(Number)
+    const [endHours, endMinutes] = end.split(':').map(Number)
+
+    const startSeconds = startHours * 3600 + startMinutes * 60
+    const endSeconds = endHours * 3600 + endMinutes * 60
+
+    return [startSeconds, endSeconds]
+  } catch {
+    return [-1, -1]
+  }
+}
+
+function getSecondsSinceMidnight(): number {
+  //@ts-ignore
+  if (window._time) {
+    //@ts-ignore
+    const [hours, minutes, seconds] = window._time.split(':')
+    const secondsSinceMidnight =
+      (parseInt(hours, 10) * 60 + parseInt(minutes, 10)) * 60 +
+      parseInt(seconds, 10)
+    return secondsSinceMidnight
+  }
+
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  // I'm not using seconds because it would cause unnecessary updates to the UI
+  return hours * 3600 + minutes * 60
+}
+
 export default function WeekdaySlide({ lessons, hours }: WeekdaySlideProps) {
   const { setPlanId } = useContext(AppContext)
+  const [currentTime, setCurrentTime] = useState(getSecondsSinceMidnight())
 
-  const entries: Entrries = hours.map((hour, i) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(getSecondsSinceMidnight())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const hoursAsTimeRanges = hours.map(getTimeRange)
+
+  const entries: Entries = hours.map((hour, i) => {
     const lessonsThisHour: Plan['timetable'][Weekday][0] = lessons[i]
 
     return {
@@ -69,7 +134,12 @@ export default function WeekdaySlide({ lessons, hours }: WeekdaySlideProps) {
                 planId: lesson.classroom.planId
               }
         }
-      })
+      }),
+      higlight: shouldBeHighlighted(
+        hoursAsTimeRanges[i],
+        i > 0 ? hoursAsTimeRanges[i - 1][1] : -1,
+        currentTime
+      )
     }
   })
 
@@ -82,7 +152,12 @@ export default function WeekdaySlide({ lessons, hours }: WeekdaySlideProps) {
               entry.centerAndRight.length > 1 ? 'small' : 'medium'
 
             return (
-              <TableRow key={i}>
+              <TableRow
+                key={i}
+                sx={{
+                  backgroundColor: entry.higlight ? 'lightcyan' : undefined
+                }}
+              >
                 <TableCell size={cellSize} className="left">
                   {entry.left}
                 </TableCell>
