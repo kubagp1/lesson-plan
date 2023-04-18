883b2ec612c2f10498f6ff356f97c200b8cf5399
kubagp <37304664+kubagp1@users.noreply.github.com>
2023-04-18 01:28:45 +0000

    add a tooltip for hiding columns

--- frontend/src/components/OptionsMenu.tsx
+++ frontend/src/components/OptionsMenu.tsx
@@ -1,14 +1,44 @@
 import MoreIcon from '@mui/icons-material/MoreVert'
-import { IconButton } from '@mui/material'
+import { IconButton, Tooltip } from '@mui/material'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
-import { useState, MouseEvent, Fragment } from 'react'
+import { useState, MouseEvent, Fragment, useEffect } from 'react'
 import HideColumnsDialog from './HideColumnsDialog'
 
 export default function OptionsMenu() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const [hideColumnsDialogOpen, setHideColumnsDialogOpen] = useState(false)
-  const open = Boolean(anchorEl)
+  const [tooltipOpen, setTooltipOpen] = useState(false)
+
+  useEffect(() => {
+    let timeout: number | undefined
+    if (localStorage.getItem('hideColumnsTooltip') === null) {
+      const handleAnyClick = () => {
+        localStorage.setItem('hideColumnsTooltip', 'true')
+        setTooltipOpen(false)
+        clearTimeout(timeout)
+      }
+
+      setTooltipOpen(true)
+
+      timeout = setTimeout(() => {
+        localStorage.setItem('hideColumnsTooltip', 'true')
+        setTooltipOpen(false)
+        document.removeEventListener('click', handleAnyClick)
+        document.removeEventListener('touchstart', handleAnyClick)
+      }, 3000)
+
+      document.addEventListener('click', handleAnyClick)
+      document.addEventListener('touchstart', handleAnyClick)
+
+      return () => {
+        clearTimeout(timeout)
+        document.removeEventListener('click', handleAnyClick)
+        document.removeEventListener('touchstart', handleAnyClick)
+      }
+    }
+  }, [])
+
   const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget)
   }
@@ -23,11 +53,34 @@ export default function OptionsMenu() {
     setHideColumnsDialogOpen(false)
   }
 
+  const open = Boolean(anchorEl)
+
   return (
     <Fragment>
-      <IconButton size="large" edge="end" color="inherit" onClick={handleClick}>
-        <MoreIcon />
-      </IconButton>
+      <Tooltip
+        arrow
+        title="Możesz pokazać więcej kolumn tutaj"
+        open={tooltipOpen}
+        PopperProps={{
+          modifiers: [
+            {
+              name: 'preventOverflow',
+              options: {
+                padding: 8
+              }
+            }
+          ]
+        }}
+      >
+        <IconButton
+          size="large"
+          edge="end"
+          color="inherit"
+          onClick={handleClick}
+        >
+          <MoreIcon />
+        </IconButton>
+      </Tooltip>
       <Menu
         anchorEl={anchorEl}
         open={open}
