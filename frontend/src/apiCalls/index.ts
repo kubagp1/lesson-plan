76853702a7371f44afc1addb1532c80da9d7450f
kubagp <37304664+kubagp1@users.noreply.github.com>
2023-04-24 10:10:19 +0000

    add support for codespaces

--- frontend/src/apiCalls/index.ts
+++ frontend/src/apiCalls/index.ts
@@ -1,8 +1,32 @@
 import { Plan, Categories } from '../shared/types'
 
+// this is so that if you port forward to localhost from codespaces, it will not try to access the codespace url
+function isAccessedFromCodespace() {
+  return (
+    import.meta.env.VITE_CODESPACES &&
+    location.hostname.endsWith(
+      import.meta.env.VITE_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+    )
+  )
+}
+
+function generateDevEntrypoint(port: number): string {
+  if (port % 1 !== 0) throw new Error('Port must be an integer')
+
+  if (isAccessedFromCodespace()) {
+    return `https://${import.meta.env.VITE_CODESPACE_NAME}-${port}.${
+      import.meta.env.VITE_GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+    }/`
+  }
+
+  return `${location.protocol}//${location.hostname}:${port}/`
+}
+
 const entrypoint = import.meta.env.PROD
   ? location.origin + '/data/'
-  : location.protocol + '//' + location.hostname + ':5000/'
+  : generateDevEntrypoint(5000)
+
+console.log({ entrypoint, env: import.meta.env })
 
 const fetchData = async (endpoint: string) => {
   const response = await fetch(entrypoint + endpoint)
