dbc93af7f4343162431c688e8ec69524628b6f66
kubagp <37304664+kubagp1@users.noreply.github.com>
2023-06-22 18:59:42 +0000

    add i18next to app

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,45 @@
+import i18n, { InitOptions } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import strings from './strings.json'
+
+// `strings` are translations from english to polish, so to make it easier to work with we derive english
+
+const resources = {
+  en: {
+    translation: Object.keys(strings).reduce(
+      (acc, key) => ({ ...acc, [key]: key }),
+      {}
+    )
+  },
+  pl: { translation: strings }
+}
+
+let initOptions: InitOptions = {
+  resources,
+  interpolation: {
+    escapeValue: false // react already safes from xss
+  },
+  saveMissing: true
+}
+
+if (import.meta.env.DEV) {
+  const debugInitOptions: InitOptions = {
+    missingKeyHandler: (...args) => {
+      console.log(`Missing translation for ${args[3]}`)
+      // @ts-ignore
+      if (window._missingTranslationAlertShown) return
+      // @ts-ignore
+      window._missingTranslationAlertShown = true
+      alert(`Missing translation for ${args[3]}`)
+    },
+    parseMissingKeyHandler: () => 'MISSING TRANSLATION'
+  }
+
+  initOptions = { ...initOptions, ...debugInitOptions }
+}
+
+i18n.use(initReactI18next).use(LanguageDetector).init(initOptions)
+
+export default i18n
